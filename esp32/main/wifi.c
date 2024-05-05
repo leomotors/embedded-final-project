1181a0d211e627273e480a07056790f02cdcc28a
Nutthapat Pongtanyavichai <59821765+leomotors@users.noreply.github.com>
2024-05-05 14:36:03 +0700

    バグ

---
+++ esp32/main/wifi.c
@@ -0,0 +1,49 @@
+#include <esp_wifi.h>
+#include <nvs_flash.h>
+
+#include "password.h"
+#include "wifi.h"
+
+static void wifi_event_handler(void *event_handler_arg,
+                               esp_event_base_t event_base, int32_t event_id,
+                               void *event_data) {
+    switch (event_id) {
+    case WIFI_EVENT_STA_START:
+        printf("WiFi connecting WIFI_EVENT_STA_START ... \n");
+        break;
+    case WIFI_EVENT_STA_CONNECTED:
+        printf("WiFi connected WIFI_EVENT_STA_CONNECTED ... \n");
+        break;
+    case WIFI_EVENT_STA_DISCONNECTED:
+        printf("WiFi lost connection WIFI_EVENT_STA_DISCONNECTED ... \n");
+        break;
+    case IP_EVENT_STA_GOT_IP:
+        printf("WiFi got IP ... \n\n");
+        break;
+    default:
+        break;
+    }
+}
+
+void init_wifi(void) {
+    nvs_flash_init();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    const wifi_init_config_t wifi_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_config));
+
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler,
+                               NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
+                               wifi_event_handler, NULL);
+
+    wifi_config_t wifi_configuration = {
+        .sta = {.ssid = WIFI_SSID, .password = WIFI_PASSWORD}};
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration));
+    esp_wifi_set_mode(WIFI_MODE_STA);
+
+    esp_wifi_start();
+    esp_wifi_connect();
+}
