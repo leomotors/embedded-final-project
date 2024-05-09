ca2a2a87d65c942a38fbf55bf6aa9f853a7d1499
Idhibhat Pankam <marginpankam@gmail.com>
2024-05-09 22:21:54 +0700

    feat: rate limiter

--- server/app/app.py
+++ server/app/app.py
@@ -2,8 +2,17 @@ import logging
 
 from flask import Flask
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"],
+    storage_uri="memory://",
+)
+
 CORS(app)
 logging.basicConfig(level=logging.INFO)
 
