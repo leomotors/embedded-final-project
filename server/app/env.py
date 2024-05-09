98a2ced7b9cd846149f48b8f0bcd72f70c7ede26
Idhibhat Pankam <marginpankam@gmail.com>
2024-05-09 22:24:56 +0700

    feat: api key warning

--- server/app/env.py
+++ server/app/env.py
@@ -1,5 +1,6 @@
 import os
 
+from app import logging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,6 +8,9 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 PORT = os.getenv("PORT")
 ENV = os.getenv("ENV")
+
+if API_KEY is None or len(API_KEY) < 6:
+    logging.warn("API_KEY is not set or is too short")
 
 
 def is_dev():
